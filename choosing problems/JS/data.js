a9ce24f9b45bd40a69de7b2f026ef68ea52d9626
sbbug <1103009779@qq.com>
2017-09-24 10:02:01 +0800

    Yes

---
+++ choosing problems/JS/data.js
@@ -0,0 +1,55 @@
+
+
+//保存图片路径信息
+
+var arrImg = new Array(20);
+
+arrImg[0]="images/1.jpg";
+arrImg[1]="images/2.jpg";
+arrImg[2]="images/3.jpg";
+arrImg[3]="images/4.jpg";
+arrImg[4]="images/5.jpg";
+arrImg[5]="images/6.jpg";
+arrImg[6]="images/7.jpg";
+arrImg[7]="images/8.jpg";
+arrImg[8]="images/9.jpg";
+arrImg[9]="images/10.jpg";
+arrImg[10]="images/11.jpg";
+arrImg[11]="images/12.jpg";
+arrImg[12]="images/13.jpg";
+arrImg[13]="images/14.jpg";
+arrImg[14]="images/15.jpg";
+arrImg[15]="images/16.jpg";
+arrImg[16]="images/17.jpg";
+arrImg[17]="images/18.jpg";
+arrImg[18]="images/19.jpg";
+arrImg[19]="images/20.jpg";
+
+//保存图片提示信息
+
+var arrImgInfo = new Array(20);
+
+arrImgInfo[0] = "LED:原理，特点，应用等";
+arrImgInfo[1] = "北纬30度：定义，地理，地质等";
+arrImgInfo[2] = "大数据：定义，特征，应用等";
+arrImgInfo[3] = "高斯：生平，相关学科贡献等";
+arrImgInfo[4] = "霍尔效应：发现者，原理，应用等";
+arrImgInfo[5] = "机器人:定义(发明者),特点，应用等";
+arrImgInfo[6] = "纳米：定义，特点，应用等";
+arrImgInfo[7] = "潜水艇:发明人，特点，应用等";
+arrImgInfo[8] = "青蒿素：发明者，应用，价值等";
+arrImgInfo[9] = "石墨烯:发明人，特性，应用等";
+arrImgInfo[10] = "探月：定义，国家，重要活动等";
+arrImgInfo[11] = "天然气：定义，形成，用途等";
+arrImgInfo[12] = "天文望远镜，定义，用途，发展等";
+arrImgInfo[13] = "网上支付:定义，功能，特征等";
+arrImgInfo[14] = "微生物:定义（发现者），特征，相关物种等";
+arrImgInfo[15] = "雾霾：定义，成分，危害等";
+arrImgInfo[16] = "信息安全:定义，相关技术及设备，事件等";
+arrImgInfo[17] = "圆周率：定义（发明者），特征，应用";
+arrImgInfo[18] = "众筹:定义，特征，发展等";
+arrImgInfo[19] = "地球：定义，地貌，结构，应用等";
+
+   
+   
+
